672c4415028c3833c8c7a0e4cb8717c03f87521e
Eric Templin <ertemplin@gmail.com>
2013-10-29 19:35:12 -0400

    Set targetSdkVersion and change app name to SafeWalk

--- android/SafeWalk/src/edu/purdue/SafeWalk/SafeWalk.java
+++ android/SafeWalk/src/edu/purdue/SafeWalk/SafeWalk.java
@@ -3,7 +3,7 @@ package edu.purdue.SafeWalk;
 import android.app.Activity;
 import android.os.Bundle;
 
-public class MainActivity extends Activity
+public class SafeWalk extends Activity
 {
     /** Called when the activity is first created. */
     @Override
