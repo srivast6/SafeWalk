378ce0da4c1176597c0dc2bfc1cb33637c1a4e8d
Eric Templin <ertemplin@gmail.com>
2013-10-31 20:34:21 -0400

    Check if Google Play Services are available
    Show the user what to do if they don't have google play services.

--- android/SafeWalk/src/edu/purdue/SafeWalk/SafeWalk.java
+++ android/SafeWalk/src/edu/purdue/SafeWalk/SafeWalk.java
@@ -5,12 +5,19 @@ import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.os.Bundle;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesUtil;
+import com.google.android.gms.common.GooglePlayServicesClient.OnConnectionFailedListener;
+import com.google.android.gms.location.LocationClient;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
 
 public class SafeWalk extends Activity
 {
 	private GoogleMap mMap;
+	private LocationClient mLocationClient;
+	
+	private final static int CONNECTION_FAILURE_RESOLUTION_REQUEST = 9000;
 	
     /** Called when the activity is first created. */
     @Override
@@ -18,8 +25,14 @@ public class SafeWalk extends Activity
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
-        mMap = ((MapFragment) getFragmentManager().findFragmentById(R.id.map)).getMap();
         
+        // Check for Google Play Services
+        int googlePlayServicesAvailable = GooglePlayServicesUtil.isGooglePlayServicesAvailable(this);
+        if(googlePlayServicesAvailable != ConnectionResult.SUCCESS) {
+        	GooglePlayServicesUtil.getErrorDialog(googlePlayServicesAvailable, this, CONNECTION_FAILURE_RESOLUTION_REQUEST).show();
+        }
+        
+        mMap = ((MapFragment) getFragmentManager().findFragmentById(R.id.map)).getMap();        
         if(mMap != null) {
         	mMap.setMyLocationEnabled(true);
         } else {
