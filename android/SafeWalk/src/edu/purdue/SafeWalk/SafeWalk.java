5ce2d60ee1e4ab2dbc586db47412109a86f59c3f
David Tschida <dmtschida1@gmail.com>
2013-11-12 22:42:52 -0500

    Moved the buttons to a fragment. Changed the sizing.

--- android/SafeWalk/src/edu/purdue/SafeWalk/SafeWalk.java
+++ android/SafeWalk/src/edu/purdue/SafeWalk/SafeWalk.java
@@ -1,14 +1,10 @@
 package edu.purdue.SafeWalk;
 
-import android.app.Activity;
 import android.app.AlertDialog;
-import android.app.AlertDialog.Builder;
 import android.content.DialogInterface;
-import android.content.Intent;
 import android.content.IntentSender.SendIntentException;
-import android.net.Uri;
 import android.os.Bundle;
-import android.view.View;
+import android.support.v4.app.FragmentActivity;
 
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.GooglePlayServicesClient;
@@ -20,95 +16,106 @@ import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
 
-public class SafeWalk extends Activity implements
-	GooglePlayServicesClient.ConnectionCallbacks,
-	GooglePlayServicesClient.OnConnectionFailedListener
+public class SafeWalk extends FragmentActivity implements
+		GooglePlayServicesClient.ConnectionCallbacks,
+		GooglePlayServicesClient.OnConnectionFailedListener
 {
 	private GoogleMap mMap;
 	private LocationClient mLocationClient;
-
+	
 	private final static int CONNECTION_FAILURE_RESOLUTION_REQUEST = 9000;
-
-    /** Called when the activity is first created. */
-    @Override
-    public void onCreate(Bundle savedInstanceState)
-    {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-
-        // Check for Google Play Services
-        int googlePlayServicesAvailable = GooglePlayServicesUtil.isGooglePlayServicesAvailable(this);
-        if(googlePlayServicesAvailable != ConnectionResult.SUCCESS) {
-        	GooglePlayServicesUtil.getErrorDialog(googlePlayServicesAvailable, this, CONNECTION_FAILURE_RESOLUTION_REQUEST).show();
-        }
-
-        mLocationClient = new LocationClient(this, this, this);
-        mLocationClient.disconnect();
-        mLocationClient.connect();
-
-        mMap = ((MapFragment) getFragmentManager().findFragmentById(R.id.map)).getMap();
-        if(mMap != null) {
-        	mMap.setMyLocationEnabled(true);
-        } else {
-        	AlertDialog.Builder alertBuilder = new AlertDialog.Builder(this);
-        	alertBuilder.setTitle("Error!");
-        	alertBuilder.setMessage(this.getResources().getText(R.string.error_no_maps));
-        	alertBuilder.setPositiveButton("Close " + this.getResources().getText(R.string.app_name), new DialogInterface.OnClickListener() {
-				@Override
-				public void onClick(DialogInterface dialog, int which) {
-					SafeWalk.this.finish();
-				}
-        	});
-        	alertBuilder.show();
-        }
-    }
-
-    /**
-     * Called when we fail to connect to the mLocationClient.
-     */
+	
+	/** Called when the activity is first created. */
 	@Override
-	public void onConnectionFailed(ConnectionResult res) {
-		if(res.isSuccess() == true)
+	public void onCreate(Bundle savedInstanceState)
+	{
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+		
+		// Check for Google Play Services
+		int googlePlayServicesAvailable = GooglePlayServicesUtil
+				.isGooglePlayServicesAvailable(this);
+		if (googlePlayServicesAvailable != ConnectionResult.SUCCESS)
+		{
+			GooglePlayServicesUtil.getErrorDialog(googlePlayServicesAvailable,
+					this, CONNECTION_FAILURE_RESOLUTION_REQUEST).show();
+		}
+		
+		mLocationClient = new LocationClient(this, this, this);
+		mLocationClient.disconnect();
+		mLocationClient.connect();
+		
+		mMap = ((MapFragment) getFragmentManager().findFragmentById(R.id.map))
+				.getMap();
+		if (mMap != null)
+		{
+			mMap.setMyLocationEnabled(true);
+		} else
+		{
+			AlertDialog.Builder alertBuilder = new AlertDialog.Builder(this);
+			alertBuilder.setTitle("Error!");
+			alertBuilder.setMessage(this.getResources().getText(
+					R.string.error_no_maps));
+			alertBuilder.setPositiveButton("Close "
+					+ this.getResources().getText(R.string.app_name),
+					new DialogInterface.OnClickListener()
+					{
+						@Override
+						public void onClick(DialogInterface dialog, int which)
+						{
+							SafeWalk.this.finish();
+						}
+					});
+			alertBuilder.show();
+		}
+	}
+	
+	/**
+	 * Called when we fail to connect to the mLocationClient.
+	 */
+	@Override
+	public void onConnectionFailed(ConnectionResult res)
+	{
+		if (res.isSuccess() == true)
 			return;
-
-		if(res.hasResolution() == true) {
-			try {
-				res.startResolutionForResult(this, CONNECTION_FAILURE_RESOLUTION_REQUEST);
-			} catch (SendIntentException e) {
+		
+		if (res.hasResolution() == true)
+		{
+			try
+			{
+				res.startResolutionForResult(this,
+						CONNECTION_FAILURE_RESOLUTION_REQUEST);
+			} catch (SendIntentException e)
+			{
 				e.printStackTrace();
-				//this.onConnectionFailed(res); //HACK: Possible stack overflow.
+				// this.onConnectionFailed(res); //HACK: Possible stack
+				// overflow.
 			}
-		} else {
+		} else
+		{
 			AlertDialog.Builder alertBuilder = new AlertDialog.Builder(this);
 			alertBuilder.setTitle("Error connection to GPS");
 			alertBuilder.setMessage("Connection result: " + res.toString());
 			alertBuilder.show();
 		}
 	}
-
+	
 	@Override
-	public void onConnected(Bundle bun) {
+	public void onConnected(Bundle bun)
+	{
 		CameraPosition.Builder cameraPositionBuilder = new CameraPosition.Builder();
-        cameraPositionBuilder.target(new LatLng(mLocationClient.getLastLocation().getLatitude(), mLocationClient.getLastLocation().getLongitude()));
-        cameraPositionBuilder.zoom(12);
-        mMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPositionBuilder.build()));
-        mLocationClient.disconnect();
+		cameraPositionBuilder.target(new LatLng(mLocationClient
+				.getLastLocation().getLatitude(), mLocationClient
+				.getLastLocation().getLongitude()));
+		cameraPositionBuilder.zoom(12);
+		mMap.animateCamera(CameraUpdateFactory
+				.newCameraPosition(cameraPositionBuilder.build()));
+		mLocationClient.disconnect();
 	}
-
+	
 	@Override
-	public void onDisconnected() {
+	public void onDisconnected()
+	{
 		mLocationClient.connect();
-	}
-
-	public void onClickSafeWalk(View view){
-		Uri number = Uri.parse("tel:7654947233");
-		Intent dial = new Intent(Intent.ACTION_DIAL, number);
-		startActivity(dial);
-	}
-
-	public void onClickPolice(View view){
-		Uri number = Uri.parse("tel:911");
-		Intent dial = new Intent(Intent.ACTION_DIAL, number);
-		startActivity(dial);
 	}
 }
