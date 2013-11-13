95470d2fa7cb08247ce78d783c15917de03a2fec
Austin Ottinger <austino45@gmail.com>
2013-11-12 19:18:23 -0500

    Fixed errors, buttons are ready to be linked to their activities

--- android/Purdue-App/src/edu/purdue/app/PurdueAppActivity.java
+++ android/Purdue-App/src/edu/purdue/app/PurdueAppActivity.java
@@ -1,13 +1,11 @@
 package edu.purdue.app;
 
 import android.app.Activity;
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.view.View.OnClickListener;
 import android.widget.Button;
 
-public class PurdueAppActivity extends Activity implements OnClickListener {
+public class PurdueAppActivity extends Activity{
     
     @Override
     public void onCreate(Bundle savedInstanceState) {
