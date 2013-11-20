e57dec58e71b8df04499a34a2f3603999db7d1a8
Kyle Potts <kyle@kptechblog.com>
2013-11-19 22:21:32 -0600

    Made a simple Dialog to pop up when a person chooses a person from the people list

--- android/SafeWalk/src/edu/purdue/SafeWalk/ListViewRequesterActivity.java
+++ android/SafeWalk/src/edu/purdue/SafeWalk/ListViewRequesterActivity.java
@@ -2,18 +2,31 @@ package edu.purdue.SafeWalk;
 
 import java.util.ArrayList;
 
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.app.DialogFragment;
 import android.app.ListActivity;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.PopupWindow;
 import android.widget.Toast;
+import android.util.Log;
 import android.view.*;
 
-public class ListViewRequesterActivity extends ListActivity {
+public class ListViewRequesterActivity extends ListActivity implements PopupDialog.NoticeDialogListener{
 	
 	static final String[] NAMES = {"Kyle", "John", "Luke", "Adam", "Sam", "Suzie", "Jessie", "James", "Meowth"};
+	public static boolean isPopupOpen = false;
+	PopupWindow popUp;
+	
+	
+
+
 	
 	public void onCreate(Bundle savedInstanceState){
 		super.onCreate(savedInstanceState);
@@ -33,11 +46,30 @@ public class ListViewRequesterActivity extends ListActivity {
 		lv.setOnItemClickListener(new OnItemClickListener() {
 			public void onItemClick(AdapterView<?> parent, View view,
 					int position, long id) {
+		        PopupDialog dialog =  new PopupDialog();
+		        dialog.show(getFragmentManager(), "NoticeDialogFragment");
+					//initPopupWindow();
 			    // When clicked, show a toast with the TextView text
-			    Toast.makeText(getApplicationContext(),
-				"This should do stuff (Not Implemented Yet)", Toast.LENGTH_SHORT).show();
+			  //  Toast.makeText(getApplicationContext(),
+				//"This should do stuff (Not Implemented Yet)", Toast.LENGTH_SHORT).show();
 			}
 		});
+		
+	}
+
+
+
+	@Override
+	public void onDialogPositiveClick(DialogFragment dialog) {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+
+	@Override
+	public void onDialogNegativeClick(DialogFragment dialog) {
+		// TODO Auto-generated method stub
 		
 	}
 	
---
+++ android/SafeWalk/src/edu/purdue/SafeWalk/PopupDialog.java
@@ -0,0 +1,54 @@
+package edu.purdue.SafeWalk;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+
+public class PopupDialog extends DialogFragment {
+	
+	public PopupDialog(){
+		super();
+	}
+
+	
+	   /* The activity that creates an instance of this dialog fragment must
+     * implement this interface in order to receive event callbacks.
+     * Each method passes the DialogFragment in case the host needs to query it. */
+    public interface NoticeDialogListener {
+        public void onDialogPositiveClick(DialogFragment dialog);
+        public void onDialogNegativeClick(DialogFragment dialog);
+    }
+    
+    // Use this instance of the interface to deliver action events
+    NoticeDialogListener mListener;
+    
+    // Override the Fragment.onAttach() method to instantiate the NoticeDialogListener
+    @Override
+    public void onAttach(Activity activity) {
+        super.onAttach(activity);
+        // Verify that the host activity implements the callback interface
+        try {
+            // Instantiate the NoticeDialogListener so we can send events to the host
+            mListener = (NoticeDialogListener) activity;
+        } catch (ClassCastException e) {
+            // The activity doesn't implement the interface, throw exception
+            throw new ClassCastException(activity.toString()
+                    + " must implement NoticeDialogListener");
+        }
+    }
+    
+    
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+    	LayoutInflater inflater = this.getActivity().getLayoutInflater();
+        // Build the dialog and set up the button click handlers
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        builder.setView(inflater.inflate(R.layout.pop_up, null));
+        return builder.create();
+    }
+}
+
+
