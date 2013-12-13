221ad164bc873ae1ca4248533f8d39ff900eac53
kylepotts <kyle@kptechblog.com>
2013-12-13 09:05:12 -0600

    Forgot to add python server to last commit

---
+++ src/edu/purdue/SafeWalk/SafeWalk.java
@@ -0,0 +1,312 @@
+package edu.purdue.SafeWalk;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.IntentSender.SendIntentException;
+import android.content.res.Configuration;
+import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.support.v4.app.ActionBarDrawerToggle;
+import android.support.v4.widget.DrawerLayout;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.View.OnTouchListener;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesClient;
+import com.google.android.gms.common.GooglePlayServicesUtil;
+import com.google.android.gms.location.LocationClient;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.UiSettings;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.LatLng;
+
+import edu.purdue.SafeWalk.TouchableWrapper.UpdateMapAfterUserInterection;
+import edu.purdue.SafeWalk.settings.SettingsActivity;
+
+public class SafeWalk extends Activity implements
+		GooglePlayServicesClient.ConnectionCallbacks,
+		GooglePlayServicesClient.OnConnectionFailedListener,
+		UpdateMapAfterUserInterection{
+	private GoogleMap mMap;
+	private LocationClient mLocationClient;
+	ListView drawerList;
+	DrawerLayout drawerLayout;
+	ActionBarDrawerToggle mDrawerToggle;
+	private final static int CONNECTION_FAILURE_RESOLUTION_REQUEST = 9000;
+
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		
+		setContentView(R.layout.main_activity);
+
+		drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+		drawerList = (ListView) findViewById(R.id.left_drawer);
+		String[] menuItems = new String[] { "Settings",
+				"What is SafeWalk?", "People", "Safewalk Personnel" };
+		drawerList.setAdapter(new ArrayAdapter<String>(this,
+				android.R.layout.simple_list_item_1, menuItems));
+		drawerList.setOnItemClickListener(new ListView.OnItemClickListener() {
+
+			@Override
+			public void onItemClick(AdapterView<?> arg0, View arg1, int order,
+					long arg3) {
+
+				if (order == 2) {
+					Intent listIntent = new Intent(SafeWalk.this,
+							ListViewRequesterActivity.class);
+					SafeWalk.this.startActivity(listIntent);
+
+				}else if(order == 0){
+					openSettings();
+
+				}else if(order == 3){
+					Intent Safewalk_Personnel = new Intent(SafeWalk.this,
+							MapPoliceActivity.class);
+					SafeWalk.this.startActivity(Safewalk_Personnel);
+				}else if(order == 1){
+					Intent Safewalk_Personnel = new Intent(SafeWalk.this,
+							AboutActivity.class);
+					SafeWalk.this.startActivity(Safewalk_Personnel);
+				} else {
+					Toast.makeText(SafeWalk.this,
+							"This feature is under construction",
+							Toast.LENGTH_SHORT).show();
+				}
+			}
+		});
+
+		final String title, drawerTitle;
+		title = drawerTitle = (String) getTitle();
+
+		drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+		mDrawerToggle = new ActionBarDrawerToggle(this, drawerLayout,
+				R.drawable.ic_drawer, R.string.drawer_open,
+				R.string.drawer_close) {
+
+			/** Called when a drawer has settled in a completely closed state. */
+			public void onDrawerClosed(View view) {
+				getActionBar().setTitle(title);
+				invalidateOptionsMenu(); // creates call to
+											// onPrepareOptionsMenu()
+			}
+
+			/** Called when a drawer has settled in a completely open state. */
+			public void onDrawerOpened(View drawerView) {
+				getActionBar().setTitle(drawerTitle);
+				invalidateOptionsMenu(); // creates call to
+											// onPrepareOptionsMenu()
+			}
+		};
+
+		// Set the drawer toggle as the DrawerListener
+		drawerLayout.setDrawerListener(mDrawerToggle);
+		getActionBar().setDisplayHomeAsUpEnabled(true);
+		getActionBar().setHomeButtonEnabled(true);
+
+		// Check for Google Play Services
+		int googlePlayServicesAvailable = GooglePlayServicesUtil
+				.isGooglePlayServicesAvailable(this);
+		if (googlePlayServicesAvailable != ConnectionResult.SUCCESS) {
+			GooglePlayServicesUtil.getErrorDialog(googlePlayServicesAvailable,
+					this, CONNECTION_FAILURE_RESOLUTION_REQUEST).show();
+		}
+
+		mLocationClient = new LocationClient(this, this, this);
+		if(mLocationClient.isConnected() == false || mLocationClient.isConnecting() == false) {
+			mLocationClient.connect();
+		}
+
+		mMap = ((MapFragment) getFragmentManager().findFragmentById(R.id.map))
+				.getMap();
+		if (mMap != null) {
+			mMap.setMyLocationEnabled(true);
+			UiSettings mapSettings = mMap.getUiSettings();
+			mapSettings.setTiltGesturesEnabled(false);
+			mapSettings.setRotateGesturesEnabled(false);
+		} else {
+			AlertDialog.Builder alertBuilder = new AlertDialog.Builder(this);
+			alertBuilder.setTitle("Error!");
+			alertBuilder.setMessage(this.getResources().getText(
+					R.string.error_no_maps));
+			alertBuilder.setPositiveButton("Close "
+					+ this.getResources().getText(R.string.app_name),
+					new DialogInterface.OnClickListener() {
+						@Override
+						public void onClick(DialogInterface dialog, int which) {
+							SafeWalk.this.finish();
+						}
+					});
+			alertBuilder.show();
+		}
+		
+		
+		View v = findViewById(R.id.mapFrame);
+		v.setOnTouchListener(new OnTouchListener() {
+
+			@Override
+			public boolean onTouch(View view, MotionEvent event) {
+				// TODO Auto-generated method stub
+				if(event.getAction() == MotionEvent.ACTION_MOVE){
+					Log.d("Awesome", "it works");
+				}
+				return false;
+			}
+			
+		});
+	}
+
+	private void openSettings() {
+		Intent setingsIntent = new Intent(SafeWalk.this, SettingsActivity.class);
+		SafeWalk.this.startActivity(setingsIntent);
+	}
+	
+	@Override
+	protected void onPause() {
+		super.onPause();
+		if (mLocationClient != null && mLocationClient.isConnected()) {
+			mLocationClient.disconnect();
+		}
+	}
+
+	/**
+	 * Called when we fail to connect to the mLocationClient.
+	 */
+	@Override
+	public void onConnectionFailed(ConnectionResult res) {
+		if (res.isSuccess() == true)
+			return;
+
+		if (res.hasResolution() == true) {
+			try {
+				res.startResolutionForResult(this,
+						CONNECTION_FAILURE_RESOLUTION_REQUEST);
+			} catch (SendIntentException e) {
+				e.printStackTrace();
+				this.onConnectionFailed(res); // HACK: Possible stack overflow.
+			}
+		} else {
+			AlertDialog.Builder alertBuilder = new AlertDialog.Builder(this);
+			alertBuilder.setTitle("Error connection to GPS");
+			alertBuilder.setMessage("Connection result: " + res.toString());
+			alertBuilder.show();
+		}
+	}
+
+	@Override
+	protected void onPostCreate(Bundle savedInstanceState) {
+		super.onPostCreate(savedInstanceState);
+		// Sync the toggle state after onRestoreInstanceState has occurred.
+		mDrawerToggle.syncState();
+	}
+	
+	@Override
+	public boolean onPrepareOptionsMenu(Menu menu) {
+		menu.findItem(R.id.action_display_blue).setChecked(PreferenceManager.getDefaultSharedPreferences(getApplicationContext()).getBoolean("show_blue_lights", false));
+		menu.findItem(R.id.action_display_vols).setChecked(PreferenceManager.getDefaultSharedPreferences(getApplicationContext()).getBoolean("show_volunteers", false));
+		
+		// If the nav drawer is open, hide action items related to the content
+		// view
+		boolean drawerOpen = drawerLayout.isDrawerOpen(drawerList);
+		// menu.findItem(R.id.action_websearch).setVisible(!drawerOpen);
+		return super.onPrepareOptionsMenu(menu);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+	    MenuInflater inflater = getMenuInflater();
+	    inflater.inflate(R.menu.map_menu, menu);
+	    return true;
+	}
+	
+	@Override
+	public void onConfigurationChanged(Configuration newConfig) {
+		super.onConfigurationChanged(newConfig);
+		mDrawerToggle.onConfigurationChanged(newConfig);
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Pass the event to ActionBarDrawerToggle, if it returns
+		// true, then it has handled the app icon touch event
+		if (mDrawerToggle.onOptionsItemSelected(item)) {
+			return true;
+		}
+		
+		SharedPreferences myPreference= PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
+		SharedPreferences.Editor editor = myPreference.edit();
+		switch (item.getItemId()) {
+        case R.id.action_display_blue:
+        	item.setChecked(!item.isChecked());
+    		editor.putBoolean("show_blue_lights", item.isChecked());
+    		editor.apply();
+            return true;
+        case R.id.action_display_vols:
+        	item.setChecked(!item.isChecked());
+    		editor.putBoolean("show_volunteers", item.isChecked());
+    		editor.apply();
+            return true;
+        case R.id.action_settings:
+        	openSettings();
+            return true;
+		}
+		
+		
+		// Handle your other action bar items...
+		return super.onOptionsItemSelected(item);
+	}
+
+	@Override
+	public void onConnected(Bundle bun) {
+		CameraPosition.Builder cameraPositionBuilder = new CameraPosition.Builder();
+		cameraPositionBuilder.target(new LatLng(mLocationClient
+				.getLastLocation().getLatitude(), mLocationClient
+				.getLastLocation().getLongitude()));
+		cameraPositionBuilder.zoom((float) 16);
+		mMap.animateCamera(CameraUpdateFactory
+				.newCameraPosition(cameraPositionBuilder.build()));
+		mLocationClient.disconnect();
+	}
+
+	@Override
+	public void onDisconnected() {
+		mLocationClient.connect();
+	}
+
+
+
+	@Override
+	public void onMapDrag() {
+		// TODO Auto-generated method stub
+		View mapPopUpLinLayout = findViewById(R.id.mapPopUpLinLayout);
+		View mapPopUpView = findViewById(R.id.mapPopUpView1);
+		mapPopUpLinLayout.setVisibility(View.INVISIBLE);
+		mapPopUpView.setVisibility(View.INVISIBLE);
+		
+	}
+
+	@Override
+	public void onMapLift() {
+		View mapPopUpLinLayout = findViewById(R.id.mapPopUpLinLayout);
+		View mapPopUpView = findViewById(R.id.mapPopUpView1);
+		mapPopUpLinLayout.setVisibility(View.VISIBLE);
+		mapPopUpView.setVisibility(View.VISIBLE);
+		// TODO Auto-generated method stub
+		
+	}
+}
---
+++ src/edu/purdue/SafeWalk/settings/SettingsFragment.java
@@ -0,0 +1,186 @@
+package edu.purdue.SafeWalk.settings;
+
+import java.util.Calendar;
+
+import android.app.Fragment;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.preference.ListPreference;
+import android.preference.Preference;
+import android.preference.PreferenceCategory;
+import android.preference.PreferenceFragment;
+import android.preference.PreferenceManager;
+import android.preference.Preference.OnPreferenceClickListener;
+import android.preference.PreferenceScreen;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Toast;
+import edu.purdue.SafeWalk.R;
+
+public class SettingsFragment extends PreferenceFragment {
+	Calendar lastVolModeClick;
+	int numVolModeClicks = 0;
+	static final int neededVolModeClicks = 7;
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+
+		setupSimplePreferencesScreen();
+
+		PreferenceScreen ps = getPreferenceScreen();
+		PreferenceCategory pc = (PreferenceCategory) ps.getPreference(ps
+				.getPreferenceCount() - 2);
+		pc.getPreference(0).setOnPreferenceClickListener(
+				new OnPreferenceClickListener() {
+					public boolean onPreferenceClick(Preference preference) {
+						if (!PreferenceManager.getDefaultSharedPreferences(
+								getActivity()).getBoolean("volunteer_mode",
+								false)) {
+							Calendar now = Calendar.getInstance();
+							if (lastVolModeClick == null) {
+								Log.d("Volunteer Mode", "First Tap");
+								numVolModeClicks++;
+								lastVolModeClick = now;
+								return false;
+							}
+
+							long timeElapsed = now.getTime().getTime()
+									- lastVolModeClick.getTime().getTime();
+							Log.d("Volunteer Mode", "Time Elapsed = "
+									+ timeElapsed);
+							if (timeElapsed < 700) {
+								numVolModeClicks++;
+								Log.d("Volunteer Mode", "Tap #"
+										+ numVolModeClicks);
+								if (numVolModeClicks == neededVolModeClicks) {
+									enableVolunteerMode();
+									numVolModeClicks = 0;
+								}
+							} else {
+								numVolModeClicks = 0;
+							}
+							lastVolModeClick = now;
+						}
+						return false;
+
+					}
+				});
+
+		Preference pref = ps.getPreference(ps.getPreferenceCount() - 1);
+		pref.setOnPreferenceClickListener(new OnPreferenceClickListener() {
+			public boolean onPreferenceClick(Preference preference) {
+				if (PreferenceManager
+						.getDefaultSharedPreferences(getActivity()).getBoolean(
+								"volunteer_mode", false)) {
+					((SettingsActivity) getActivity())
+							.swapFragments(new VolunteerSettingsFragment());
+					return true;
+				}
+				return false;
+			}
+		});
+	}
+
+	@Override
+	public void onResume() {
+		super.onResume();
+		Log.d("SettingsFragment", "onResume()");
+		PreferenceScreen ps = getPreferenceScreen();
+		Preference pref = ps.getPreference(ps.getPreferenceCount() - 1);
+		pref.setEnabled(PreferenceManager.getDefaultSharedPreferences(
+				getActivity()).getBoolean("volunteer_mode", false));
+	}
+
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+		View view = super.onCreateView(inflater, container, savedInstanceState);
+		view.setBackgroundColor(getResources().getColor(android.R.color.black));
+
+		return view;
+	}
+
+	public void enableVolunteerMode() {
+		Log.d("Volunteer Mode", "Volunteer mode enabled!!!");
+		Toast.makeText(getActivity().getApplicationContext(),
+				"Volunteer Mode Enabled!", Toast.LENGTH_LONG).show();
+
+		SharedPreferences myPreference = PreferenceManager
+				.getDefaultSharedPreferences(getActivity());
+		SharedPreferences.Editor editor = myPreference.edit();
+		editor.putBoolean("volunteer_mode", true);
+		editor.apply();
+
+		PreferenceScreen ps = getPreferenceScreen();
+		Preference pref = ps.getPreference(ps.getPreferenceCount() - 1);
+		pref.setEnabled(true);
+	}
+
+	/**
+	 * Shows the simplified settings UI if the device configuration if the
+	 * device configuration dictates that a simplified, single-pane UI should be
+	 * shown.
+	 */
+	private void setupSimplePreferencesScreen() {
+
+		addPreferencesFromResource(R.xml.preferences);
+
+		bindPreferenceSummaryToValue(findPreference("pref_loc_update_frequency"));
+	}
+
+	/**
+	 * Binds a preference's summary to its value. More specifically, when the
+	 * preference's value is changed, its summary (line of text below the
+	 * preference title) is updated to reflect the value. The summary is also
+	 * immediately updated upon calling this method. The exact display format is
+	 * dependent on the type of preference.
+	 * 
+	 * @see #sBindPreferenceSummaryToValueListener
+	 */
+
+	private void bindPreferenceSummaryToValue(Preference preference) {
+		// Set the listener to watch for value changes.
+		preference
+				.setOnPreferenceChangeListener(sBindPreferenceSummaryToValueListener);
+
+		// Trigger the listener immediately with the preference's
+		// current value.
+		sBindPreferenceSummaryToValueListener.onPreferenceChange(
+				preference,
+				PreferenceManager.getDefaultSharedPreferences(
+						preference.getContext()).getString(preference.getKey(),
+						""));
+	}
+
+	/**
+	 * A preference value change listener that updates the preference's summary
+	 * to reflect its new value.
+	 */
+	private Preference.OnPreferenceChangeListener sBindPreferenceSummaryToValueListener = new Preference.OnPreferenceChangeListener() {
+		@Override
+		public boolean onPreferenceChange(Preference preference, Object value) {
+			String stringValue = value.toString();
+
+			if (preference instanceof ListPreference) {
+				// For list preferences, look up the correct display value
+				// in
+				// the preference's 'entries' list.
+				ListPreference listPreference = (ListPreference) preference;
+				int index = listPreference.findIndexOfValue(stringValue);
+
+				// Set the summary to reflect the new value.
+				preference
+						.setSummary(index >= 0 ? listPreference.getEntries()[index]
+								: null);
+			} else {
+				// For all other preferences, set the summary to the value's
+				// simple string representation.
+				preference.setSummary(stringValue);
+			}
+			return true;
+		}
+	};
+}
