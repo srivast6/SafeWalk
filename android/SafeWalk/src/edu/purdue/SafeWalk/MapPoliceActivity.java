9e5b3ec23badc4789447d5be4b3af6eee8559b7c
David Tschida <dmtschida1@gmail.com>
2013-12-17 01:51:41 -0500

    Added some more pins to the Personnel map.

--- android/SafeWalk/src/edu/purdue/SafeWalk/MapPoliceActivity.java
+++ android/SafeWalk/src/edu/purdue/SafeWalk/MapPoliceActivity.java
@@ -38,11 +38,47 @@ public class MapPoliceActivity extends SafeWalk {
 			alertBuilder.show();
 		}
 		
-		mapPolice.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(41.889,
-				-87.622), 16));
+		mapPolice.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(40.4173698, -86.8765488), 16));
 
 		// You can customize the marker image using images bundled with
 		// your app, or dynamically generated bitmaps.
+		mapPolice.addMarker(new MarkerOptions()
+		.icon(BitmapDescriptorFactory
+				.fromResource(R.drawable.map_pin))
+		.anchor(0.0f, 1.0f) // Anchors the marker on the bottom left
+		.position(new LatLng(40.4183698, -86.8765388)));
+		
+		mapPolice.addMarker(new MarkerOptions()
+		.icon(BitmapDescriptorFactory
+				.fromResource(R.drawable.map_pin))
+		.anchor(0.0f, 1.0f) // Anchors the marker on the bottom left
+		.position(new LatLng(40.5183678, -86.8265378)));
+		
+		mapPolice.addMarker(new MarkerOptions()
+		.icon(BitmapDescriptorFactory
+				.fromResource(R.drawable.map_pin))
+		.anchor(0.0f, 1.0f) // Anchors the marker on the bottom left
+		.position(new LatLng(41.4183698, -86.8765300)));
+		
+		mapPolice.addMarker(new MarkerOptions()
+		.icon(BitmapDescriptorFactory
+				.fromResource(R.drawable.map_pin))
+		.anchor(0.0f, 1.0f) // Anchors the marker on the bottom left
+		.position(new LatLng(40.4083698, -86.8769088)));
+		
+		mapPolice.addMarker(new MarkerOptions()
+		.icon(BitmapDescriptorFactory
+				.fromResource(R.drawable.map_pin))
+		.anchor(0.0f, 1.0f) // Anchors the marker on the bottom left
+		.position(new LatLng(40.4183608, -86.8165385)));
+		
+		mapPolice.addMarker(new MarkerOptions()
+		.icon(BitmapDescriptorFactory
+				.fromResource(R.drawable.map_pin))
+		.anchor(0.0f, 1.0f) // Anchors the marker on the bottom left
+		.position(new LatLng(40.4180697, -86.1765378)));
+		
+		
 		mapPolice.addMarker(new MarkerOptions()
 		.icon(BitmapDescriptorFactory
 				.fromResource(R.drawable.map_pin))
