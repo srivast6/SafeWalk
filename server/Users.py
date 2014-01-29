46fc80f442c50c5752e9830ee77ab0d435d88cbe
Eric Templin <ertemplin@gmail.com>
2014-01-28 20:20:17 -0500

    Implement simple user storage and URL

--- server/Users.py
+++ server/Users.py
@@ -1,4 +1,5 @@
 import webapp2
+import json
 from google.appengine.ext import ndb
 
 class User(ndb.Model):
@@ -9,6 +10,7 @@ class User(ndb.Model):
     deviceType = ndb.StringProperty() # 'iOS' or 'Android'
     deviceToken = ndb.StringProperty() # 32 bytes of data on iOS, String on Android (GCM)
     purdueCASServiceTicket = ndb.StringProperty() # Set https://www.purdue.edu/apps/account/html/cas_presentation_20110407.pdf
+    isAdminUser = ndb.BooleanProperty()
 
 class UsersHandler(webapp2.RequestHandler):
     def head(self):
@@ -18,11 +20,26 @@ class UsersHandler(webapp2.RequestHandler):
     # Mobile app sends post to create new user
     def post(self):
         self.response.status = 200
-        self.response.write("Nothing to see here yet!")
+        newUser = User(name=self.request.get("name"),
+                phoneNumber=self.request.get("phoneNumber"),
+                currentLocation_lat=self.request.get("currentLocation_lat"),
+                currentLocation_lng=self.request.get("currentLocation_lng"),
+                deviceType="Android", #TODO: Get this from the device
+                deviceToken=self.request.get("deviceToken"),
+                purdueCASServiceTicket=self.request.get("purdueCASServiceTicket"),
+                isAdminUser=False)
+        newUser.put()
+
 
     # Returns list of all users
     def get(self):
         self.response.status = 200
-        self.response.write("Nothing to see yet!")
+        qry = User.query()
+        numToFetch = qry.count()
+        results = qry.fetch(numToFetch)
+        users = []
+        for user in results:
+            users.append(user.to_dict())
+        self.response.write(json.dumps(users))
 
 
