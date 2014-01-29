d6201c01e78d55c182003a72aea8810cbc4e0a06
Aaron Peters <SIGAPP@pal-nat184-066-100.itap.purdue.edu>
2014-01-28 19:15:26 -0500

    Adding the folder which will contain our ios source code

---
+++ ios/SafeWalk/SafeWalk/MasterViewController.h
@@ -0,0 +1,13 @@
+//
+//  MasterViewController.h
+//  SafeWalk
+//
+//  Created by Aaron Peters on 1/28/14.
+//  Copyright (c) 2014 Aaron Peters. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MasterViewController : UITableViewController
+
+@end
