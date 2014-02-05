2aee2765045f06d4f6609de1f5019f34fd2bd499
Eric Templin <ertemplin@gmail.com>
2014-02-04 19:21:34 -0500

    Re-create ios SafeWalk project

---
+++ iOS/SafeWalk/SafeWalk/ACMAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  ACMAppDelegate.h
+//  SafeWalk
+//
+//  Created by Eric Templin on 2/4/14.
+//  Copyright (c) 2014 Eric Templin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ACMAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
--- iOS/SafeWalk/SafeWalk/ACMAppDelegate.m
+++ iOS/SafeWalk/SafeWalk/ACMAppDelegate.m
@@ -1,21 +1,24 @@
 //
-//  AppDelegate.m
+//  ACMAppDelegate.m
 //  SafeWalk
 //
-//  Created by Aaron Peters on 1/28/14.
-//  Copyright (c) 2014 Aaron Peters. All rights reserved.
+//  Created by Eric Templin on 2/4/14.
+//  Copyright (c) 2014 Eric Templin. All rights reserved.
 //
 
-#import "AppDelegate.h"
+#import "ACMAppDelegate.h"
 
-@implementation AppDelegate
+@implementation ACMAppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
+    self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
     // Override point for customization after application launch.
+    self.window.backgroundColor = [UIColor whiteColor];
+    [self.window makeKeyAndVisible];
     return YES;
 }
-							
+
 - (void)applicationWillResignActive:(UIApplication *)application
 {
     // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
