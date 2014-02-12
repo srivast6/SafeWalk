61326327a51b3ac35912cfe05e30c6a410415fcb
Eric Templin <ertemplin@gmail.com>
2014-02-11 22:49:24 -0500

    Notify every user if a request is accepted

--- server/getRequestHandler.py
+++ server/getRequestHandler.py
@@ -4,7 +4,9 @@ import logging
 import json
 from Request import Request
 from google.appengine.api.logservice import logservice
+from gcm import GCM
 from RequestHandler import RequestHandler
+from Users import User
 
 
 class getRequestHandler(webapp2.RequestHandler):
@@ -14,9 +16,9 @@ class getRequestHandler(webapp2.RequestHandler):
 
 
     # this will be a request from the app for information, server will send json to app.     
-    def get(self,id):
+    def get(self, requestId=""):
         """Respond to a GET request."""
-        user = self.getUserByUUID(id)
+        user = self.getRequestByUUID(requestId)
         user.requestAccepted = True
         user.put()
         logging.info("rquestAccepted = %s" %str(user.requestAccepted))
@@ -37,17 +39,37 @@ class getRequestHandler(webapp2.RequestHandler):
                         """ % ("test","test")
         self.response.write(response)
         
+        # Send everyone a push notification about the request being accepted
+        gcm = GCM('AIzaSyC6y8uyxPSjxPABKieRa2iB2wLxFVyJuQY')
+        data = {'param1': 'value1', 'param2': 'value2', 'appName': 'SafeWalk'}
+        users = User.query().fetch()
+        gcm_ids = []
+        for user in users:
+            logging.info("user = %s" %str(user))
+            logging.info("this gcm_id = %s" %str(user.gcmID))
+            gcm_ids.append(user.gcmID)
+        logging.info("gcm_ids = %s" %str(gcm_ids))
+        response = gcm.json_request(registration_ids=gcm_ids, data=data)
+        
         
 
     #respond to POST Request, which will come from Safewalk App
-    def post(self):
-        url = self.request.url[-36:]
-        user = self.getUserByUUID(url)
-        user.requestAccepted = True
+    def post(self, requestId=""):
+        request = self.getRequestByUUID(requestId)
+        request.requestAccepted = True
         self.response.status = 200
 
+        # Send everyone a push notification about the request being accepted
+        gcm = GCM('AIzaSyC6y8uyxPSjxPABKieRa2iB2wLxFVyJuQY')
+        data = {'param1': 'value1', 'param2': 'value2', 'appName': 'SafeWalk'}
+        users = User.query().fetch()
+        gcm_ids = []
+        for user in users:
+            gcm_ids.append(user.gcmID)
+        response = gcm.json_request(registration_ids=gcm_ids, data=data)
 
-    def getUserByUUID(self,id):
+
+    def getRequestByUUID(self,id):
         match = Request.query(Request.requestId == id).fetch()
         logging.info("match %s" %str(match[0]))
         return match[0]
