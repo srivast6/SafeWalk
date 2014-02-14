44aced1d72eedeacfa104ea76669ca2d23e250c1
David Tschida <dmtschida1@gmail.com>
2014-02-13 23:00:51 -0500

    BROKEN layouts

--- android/SafeWalk/src/edu/purdue/SafeWalk/TouchableWrapper.java
+++ android/SafeWalk/src/edu/purdue/SafeWalk/TouchableWrapper.java
@@ -2,6 +2,7 @@ package edu.purdue.SafeWalk;
 
 import android.content.Context;
 import android.os.SystemClock;
+import android.util.AttributeSet;
 import android.view.MotionEvent;
 import android.widget.FrameLayout;
 
@@ -10,21 +11,31 @@ import android.widget.FrameLayout;
  * 
  */
 
-public  class TouchableWrapper extends FrameLayout {
+public class TouchableWrapper extends FrameLayout {
+
+	
+
+	public TouchableWrapper(Context context) {
+		super(context);
+		// TODO Auto-generated constructor stub
+	}
+	public TouchableWrapper(Context context, AttributeSet attrs) {
+		super(context, attrs);
+		// TODO Auto-generated constructor stub
+	}
+	public TouchableWrapper(Context context, AttributeSet attrs, int defStyle) {
+		super(context, attrs, defStyle);
+		// TODO Auto-generated constructor stub
+	}
 
 	private long lastTouched = 0;
-	private static final long SCROLL_TIME = 5L; // 200 Milliseconds, but you can adjust that to your liking
+	private static final long SCROLL_TIME = 5L; // 200 Milliseconds, but you can
+												// adjust that to your liking
 	private UpdateMapAfterUserInterection updateMapAfterUserInterection;
 
-	public TouchableWrapper(MapOverlayHandler mapOverlayHandler) {
-		super(mapOverlayHandler.getContext());
-		// Force the host activity to implement the UpdateMapAfterUserInterection Interface
-		try {
-			updateMapAfterUserInterection = mapOverlayHandler;
-        } catch (ClassCastException e) {
-            throw new ClassCastException(mapOverlayHandler.getContext().toString() + " must implement UpdateMapAfterUserInterection");
-        }
-	}
+//	public TouchableWrapper(MapOverlayHandler mapOverlayHandler) {
+//		super(mapOverlayHandler.getContext());
+//	}
 
 	@Override
 	public boolean dispatchTouchEvent(MotionEvent ev) {
@@ -40,9 +51,22 @@ public  class TouchableWrapper extends FrameLayout {
 	}
 
 	// Map Activity must implement this interface
-    public interface UpdateMapAfterUserInterection {
-    	public void onMapDrag();
-    	public void onMapLift();
-    
-    }
+	public interface UpdateMapAfterUserInterection {
+		public void onMapDrag();
+
+		public void onMapLift();
+
+	}
+
+	public void setOverlayHandler(MapOverlayHandler mapOverlayHandler) {
+		// Force the host activity to implement the
+		// UpdateMapAfterUserInterection Interface
+		try {
+			updateMapAfterUserInterection = mapOverlayHandler;
+		} catch (ClassCastException e) {
+			throw new ClassCastException(mapOverlayHandler.getContext()
+					.toString()
+					+ " must implement UpdateMapAfterUserInterection");
+		}
+	}
 }
