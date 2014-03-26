ad98dc89ba7cbdf7a7c84101f61c24223a06259a
Aaron Peters <peter177@purdue.edu>
2014-03-25 23:29:13 -0400

    Added blank ViewControllers for the settings table

---
+++ iOS/SafeWalk/SafeWalk/ACMSettingsViewController.h
@@ -0,0 +1,13 @@
+//
+//  ACMSettingsViewController.h
+//  SafeWalk
+//
+//  Created by Aaron Peters on 3/25/14.
+//  Copyright (c) 2014 Eric Templin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ACMSettingsViewController : UITableViewController
+
+@end
