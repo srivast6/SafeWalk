eb737fa0ab1116447372b61299fa4b8eb1909ad9
Eric Templin <ertemplin@gmail.com>
2014-11-05 17:40:45 -0500

    Add google maps and call buttons to swift app

--- iOS/SafeWalk/SafeWalk/AppDelegate.swift
+++ iOS/SafeWalk/SafeWalk/AppDelegate.swift
@@ -16,6 +16,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
+        GMSServices.provideAPIKey("AIzaSyC9wZLqc-ctLwevGnSA0w3vYEmIssxRq70");
         return true
     }
 
--- iOS/SafeWalk/SafeWalk/ViewController.swift
+++ iOS/SafeWalk/SafeWalk/ViewController.swift
@@ -9,15 +9,45 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var mapView: GMSMapView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        var camera = GMSCameraPosition.cameraWithLatitude(40.427605, longitude:-86.916962, zoom:17)
+        mapView.camera = camera
+        
+        mapView.myLocationEnabled = true
+        mapView.settings.myLocationButton = true
+        mapView.settings.tiltGestures = false
+        mapView.settings.rotateGestures = false
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func callSafeWalkButtonPressed(UIButton) {
+        self.callPhoneNumber("tel:765-494-7233")
+    }
+    
+    @IBAction func callPoliceButtonPressed(UIButton) {
+        self.callPhoneNumber("tel:411")
+    }
+    
+    func callPhoneNumber(phoneURL:NSString) {
+        var callNumberURL = NSURL(string: phoneURL)
+        if(UIApplication.sharedApplication().canOpenURL(callNumberURL!)) {
+            UIApplication.sharedApplication().openURL(callNumberURL!);
+        } else {
+            var alertView = UIAlertView()
+            alertView.title = "Error"
+            alertView.message = "Your device doesn't support this feature."
+            alertView.addButtonWithTitle("OK")
+            alertView.show()
+        }
     }
 
 
